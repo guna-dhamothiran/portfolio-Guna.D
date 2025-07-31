c84afe0004e700cc70d1dacef0aa17b8fce49faa
GUNA D <gunad.22it@kongu.edu>
2025-07-31 20:29:19 +0530

    Update aboutData.js

--- Portfolio/src/data/aboutData.js
+++ Portfolio/src/data/aboutData.js
@@ -1,7 +1,7 @@
 export const aboutData = {
   title: "Who I am",
   description1:
-    "I'm a Third-year Information Technology student at Kongu Engineering College Engineering and Technology, perudurai. ",
+    "I'm a final year Information Technology student at Kongu Engineering College Engineering and Technology, perudurai. ",
   description2:
     "Currently seeking full-time and internship opportunities to apply my skills and knowledge.",
   image: 1,
